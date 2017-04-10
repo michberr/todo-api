5ffa21f9924babbe3296880b54e736e2fbce160b
Michelle Berry <michberr8@gmail.com>
2017-04-10 13:52:28 -0700

    Added a put method and args-parsing

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask, request, abort
+from flask_restful import Resource, Api, reqparse
 from models import Todo, db, connect_to_db
 
 
@@ -7,33 +7,79 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
+#####################################################
+#               Define Resources                    #
+#####################################################
+
 
 class TodoView(Resource):
+
+    def __init__(self):
+        """Parse arguments from json"""
+
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('name',
+                                 type=str,
+                                 required=True,
+                                 help='No name provided',
+                                 location='json')
+        self.parser.add_argument('description',
+                                 type=str,
+                                 default="",
+                                 location='json')
+
     def get(self, todo_id):
         """Retrieve a todo item by id"""
 
         todo = Todo.query.get(todo_id)
+
+        if not todo:
+            abort(404, "That id does not exist in the database")
+
         return todo.as_dict()
 
     def post(self):
         """Create a new todo item"""
 
-        raw_dict = request.get_json()
+        args = self.parser.parse_args()
+        print args
 
-        todo = Todo(name=raw_dict['name'],
-                    description=raw_dict['description'])
+        todo = Todo(name=args['name'],
+                    description=args['description'])
 
         db.session.add(todo)
         db.session.commit()
 
         return todo.as_dict()
 
+    def put(self, todo_id):
+        """Update a todo item"""
+
+        args = self.parser.parse_args()
+
+        todo = Todo.query.get(todo_id)
+
+        if not todo:
+            abort(404, "That id does not exist in the database")
+
+        todo.name = args["name"]
+        todo.description = args["description"]
+        db.session.commit()
+
+        return todo.as_dict()
+
+
+#####################################################
+#                Add resources to API               #
+#####################################################
 api.add_resource(TodoView,
                  '/todo/',
                  '/todo/<int:todo_id>')
 
 
-###############################################################
+#####################################################
+#                      Run App                      #
+#####################################################
 if __name__ == '__main__':
 
     connect_to_db(app)
